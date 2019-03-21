845d568222a91c51d29cf5f13c7c7e93fc973b97
Dmitrii Savchenko <dsavax@gmail.com>
2019-03-22 00:17:45 +0300

    add in app.js runtime log: date, method, original URL

--- servers/web/app.js
+++ servers/web/app.js
@@ -38,6 +38,11 @@ app.use(async (request, response, next) => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use((request, response, next) => {
+  console.log(new Date().toISOString(), request.method, request.originalUrl);
+  return next();
+});
+
 app.use(require('./router'));
 
 module.exports = app;
